49e0a802fa367c6da00a2f8389237798ee62dea4
Mohamed Alaa <malaaosmanosman5260@gmail.com>
2024-11-20 13:34:47 +0200

    set up for project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
+import { TrafficComponent } from "./dashboard/traffic/traffic.component";
+import { TicketsComponent } from "./dashboard/tickets/tickets.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent],
 })
 export class AppComponent {
   title = 'traffic-data';
+
 }
